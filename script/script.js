21bada6051f3136c2bf57d4ab54f57aa461182b5
ahmed-thisworld <168841517+ahmed-thisworld@users.noreply.github.com>
2024-05-05 00:30:04 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,16 +1,23 @@
 const collapseExpandBtn = document.querySelector("#btn-collapse-expand")
+const backtoTop = document.querySelector(".back-to-top")
 const header = document.querySelector("header .links")
 collapseExpandBtn.addEventListener("click", () => {
     header.classList.toggle("hide")
 })
-// document.addEventListener("DOMContentLoaded", () => {
-//     const circle= document.querySelector(".back-to-top .circle")
-//     window.addEventListener("scroll", () => {
-//         const height = window.innerHeight
-//         const fullHeight = document.body.clientHeight
-//         const scrolled= window.scrollY
-//         const progress = (scrolled / (fullHeight - height)) * 100
+document.addEventListener("DOMContentLoaded", () => {
+    // const circle= document.querySelector(".back-to-top .circle")
+    window.addEventListener("scroll", () => {
+        console.log(window.scrollY,window.innerHeight)
+        if(window.scrollY > window.innerHeight - 150) {
+            backtoTop.classList.remove("hide")
+        }else {
+            backtoTop.classList.add("hide")
+        }
+        // const height = window.innerHeight
+        // const fullHeight = document.body.clientHeight
+        // const scrolled= window.scrollY
+        // const progress = (scrolled / (fullHeight - height)) * 100
 
-//         circle.style.transform = `rotate(${(progress * 3.6) - 90}deg)`
-//     })
-// })
+        // circle.style.transform = `rotate(${(progress * 3.6) - 90}deg)`
+    })
+})
