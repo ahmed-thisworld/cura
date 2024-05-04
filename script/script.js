bbc800305de0922bfe96b4192b09ea3ed88dae4d
ahmed-thisworld <168841517+ahmed-thisworld@users.noreply.github.com>
2024-05-05 00:17:52 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,14 +3,14 @@ const header = document.querySelector("header .links")
 collapseExpandBtn.addEventListener("click", () => {
     header.classList.toggle("hide")
 })
-document.addEventListener("DOMContentLoaded", () => {
-    const circle= document.querySelector(".back-to-top .circle")
-    window.addEventListener("scroll", () => {
-        const height = window.innerHeight
-        const fullHeight = document.body.clientHeight
-        const scrolled= window.scrollY
-        const progress = (scrolled / (fullHeight - height)) * 100
+// document.addEventListener("DOMContentLoaded", () => {
+//     const circle= document.querySelector(".back-to-top .circle")
+//     window.addEventListener("scroll", () => {
+//         const height = window.innerHeight
+//         const fullHeight = document.body.clientHeight
+//         const scrolled= window.scrollY
+//         const progress = (scrolled / (fullHeight - height)) * 100
 
-        circle.style.transform = `rotate(${(progress * 3.6) - 90}deg)`
-    })
-})
+//         circle.style.transform = `rotate(${(progress * 3.6) - 90}deg)`
+//     })
+// })
